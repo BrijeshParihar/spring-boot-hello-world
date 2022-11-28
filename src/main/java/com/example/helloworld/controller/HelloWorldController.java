2822e2e1fbb7b5b3ff3745f7ae01ec629fc20d78
BrijeshParihar <80513034+BrijeshParihar@users.noreply.github.com>
2022-11-28 21:40:17 +0530

    Update HelloWorldController.java

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String sendGreetings() {
-        return "Hello, World!";
+        return "<h1>I am runing.....</h1>";
     }
 }
